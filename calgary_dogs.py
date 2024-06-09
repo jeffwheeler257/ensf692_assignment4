f5434303b15118dfc09fa7d5c76fb3dac8760d2e
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2024-06-09 09:51:46 -0600

    Work on code

--- calgary_dogs.py
+++ calgary_dogs.py
@@ -16,11 +16,48 @@ def main():
     dog_data = pd.read_excel(".\CalgaryDogBreeds.xlsx")
 
     print("ENSF 692 Dogs of Calgary")
-    print(dog_data.head(10))
 
     # User input stage
 
+    while(True):
+        dog_breed = input("Please enter a dog breed: ").upper()
+        try:
+            if dog_breed in dog_data.values:
+                break
+            else:
+                raise KeyError
+        except KeyError:
+            print("Dog breed not found in the data. Please try again.")
+            continue
+
     # Data anaylsis stage
+
+    # Find and print all years where the selected breed was listed in the top breeds.
+    years = dog_data.loc[dog_data.Breed == dog_breed, ['Year']]['Year'].unique()
+    print(f"The {dog_breed} was found in the top breeds for years: {years}")
+
+    # Calculate and print the total number of registrations of the selected breed found in the dataset.
+    total = dog_data.loc[dog_data.Breed == dog_breed, ['Total']]['Total'].sum()
+    print(f"There have been {total} {dog_breed} dogs registered total.")
+
+    # Calculate and print the percentage of selected breed registrations out of the total percentage for each year (2021, 2022, 2023).
+    # Calculate and print the percentage of selected breed registrations out of the total three-year percentage
+    num_of_dogs_2021 = dog_data.groupby(['Year']).sum().loc[2021,'Total']
+    num_of_dogs_2022 = dog_data.groupby(['Year']).sum().loc[2022,'Total']
+    num_of_dogs_2023 = dog_data.groupby(['Year']).sum().loc[2023,'Total']
+    num_of_dogs = num_of_dogs_2021 + num_of_dogs_2022 + num_of_dogs_2023
+    breed_2021 = dog_data.loc[dog_data.Breed == dog_breed, ['Year', 'Total']].groupby(['Year']).sum().loc[2021,'Total']
+    breed_2022 = dog_data.loc[dog_data.Breed == dog_breed, ['Year', 'Total']].groupby(['Year']).sum().loc[2022, 'Total']
+    breed_2023 = dog_data.loc[dog_data.Breed == dog_breed, ['Year', 'Total']].groupby(['Year']).sum().loc[2023, 'Total']
+    breed_tot = breed_2021 + breed_2022 + breed_2023
+
+    print(f"The {dog_breed} was {round(breed_2021/num_of_dogs_2021 * 100, 6)}% of top breeds in 2021.")
+    print(f"The {dog_breed} was {round(breed_2022/num_of_dogs_2022 * 100, 6)}% of top breeds in 2021.")
+    print(f"The {dog_breed} was {round(breed_2023/num_of_dogs_2023 * 100, 6)}% of top breeds in 2021.")
+    print(f"The {dog_breed} was {round(breed_tot/num_of_dogs * 100, 6)}% of top breeds across all years.")
+
+    # Find and print the months that were most popular for the selected breed registrations. Print all months that tie
+
 
 if __name__ == '__main__':
     main()
